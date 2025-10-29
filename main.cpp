3ba7127f1c6ec814bb3cfd21aa1fac58bd614657
suithomas <250088@newuu.uz>
2025-10-29 12:56:36 +0500

    i cooked

---
+++ main.cpp
@@ -0,0 +1,81 @@
+ #include<iostream>
+ using namespace std;
+ int main() {
+//     //arrays
+//     //problem1
+//     //     int n,sum=0;
+//     //     cout<<"Enter number of numbers"<<endl;
+//     //     cin>>n;
+//     //     int array[n];
+//     //     for(int i=0;i<n;i++){
+//     //         cout<<"Enter "<<i+1<<"- number: ";
+//     //         cin>>array[i];}
+//     //
+//     //
+//     //     for(int i=0;i<n;i++) {
+//     //         sum=sum+array[i];
+//     //
+//     //     }
+//     //
+//     // cout<<"Sum is "<<sum<<endl;}
+//     //problem2
+//     // int n,sum=0;
+//     // cout<<"Enter number of numbers"<<endl;
+//     // cin>>n;
+//     // int array[n];
+//     // for(int i=0;i<n;i++){
+//     //     cout<<"Enter "<<i+1<<"- number: ";
+//     //     cin>>array[i];}
+//     //
+//     //
+//     // for(int i=0;i<n;i++) {
+//     //     sum=sum+array[i];
+//     //
+//     // }
+//     //
+//     // cout<<"Average=  "<<float(sum)/n<<endl;}
+//
+//     //problem3
+//
+//     float min(int n ){
+//         float array2[n];
+//         for (int i = 0; i < n; i++) {
+//             cout<<"enter"<<i+1<<endl;
+//             cin>>array2[i];
+//
+//         }
+//         float min=array2[0];
+//         for(int i=1;i<n;i++) {
+//             if(array2[i]<min) {
+//                 min=array2[i];
+//             }
+//         }
+//     }
+// }
+
+
+
+
+// int countEvenDigits(int n) {
+//
+//     if (n == 0)
+//         return 0;
+//
+//     int lastDigit = n % 10;
+//     int isEven = (lastDigit % 2 == 0) ? 1 : 0;
+//
+//
+//     return isEven + countEvenDigits(n / 10);
+// }
+//
+// int main() {
+//     int n;
+//     cout << "Enter a number: ";
+//     cin >> n;
+//
+//     cout << "Number of even digits: " << countEvenDigits(n) << endl;
+//
+//     return 0;
+//}
+   //problem11
+   int n,target;
