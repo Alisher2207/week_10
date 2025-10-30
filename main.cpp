37847546fa66fee715b0708dc9b5434f0f5f2a22
250087 <250087@newuu.uz>
2025-10-30 12:57:39 +0500

    did

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
  #include<iostream>
  using namespace std;
- int main() {
+ //int main() {
 //     //arrays
 //     //problem1
 //     //     int n,sum=0;
@@ -77,5 +77,64 @@
 //
 //     return 0;
 //}
-   //problem11
-   int n,target;
+   //problem4
+//
+//    int indexOfLargestElement(double array[], int size){
+// int maxIndex = 0 ;
+// for (int i = 0; i < size; i++) {
+//     if (array[i] >= array[maxIndex]){
+//         maxIndex = i;
+//     }
+// }
+// return maxIndex;
+// }
+// int main() {
+//     double numbers[15];
+//     cout<<"Enter 15 numbers : ";
+//     for( int i = 0; i < 15; i++){
+//         cin>>numbers[i];
+//     }
+//     cout<<"Largest element : "<<indexOfLargestElement(numbers, 15)<<endl;
+//     return 0;
+// }
+
+// //problem5
+// bool strictlyEqual(const int list1[], const int list2[],int size ) {
+//     for (int i = 0; i < size; i++) {
+//         if (list1[i] != list2[i]) return false;
+//     }
+//     return true;
+//     }
+// int main() {
+//     int n; cout<<"Size = "; cin>>n;
+//     int list1[n];
+//     for (int i = 0; i<n; i++) cin>>list1[i];
+//     int list2[n];
+//     for (int i = 0; i<n; i++) cin>>list2[i];
+//     if (strictlyEqual(list1,list2,n)) cout<<5"Two lists are strictly identical"<<endl;
+//     else cout<<"Two lists are not identical"<<endl;
+//     return 0;
+// }
+//problem6
+// int n6;
+// cin >> n6;
+// int upp = 0, low = 0, digit = 0, special = 0;
+// char array6[n6];
+// for (int i= 0;i< n6; i++) {
+//     cin>>array6[i];
+// }
+//     for (int  i = 0; i < n6; i++) {
+//     if ( 97 <=array6[i] && array6[i] <= 122) {
+//         low+=1;
+//     } else if (65 <= array6[i] && array6[i]<= 90) {
+//         upp+= 1;
+//     } else if (48 <= array6[i] && array6[i] <= 57) {
+//         digit +=1;
+//
+//     } else {
+//         special += 1;
+//     }
+//
+// }
+// cout <<"Uppercase:" << upp<<"\nLowercase:"<<low/,/,"\nDigit:"<<digit<<"\nSpecial:"<<special;
+//problem7
